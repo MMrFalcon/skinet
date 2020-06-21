7fb67e3a92276947990e3591279f0b5aeb197ab5
MMrFalcon <kubasokol123456@gmail.com>
2020-06-21 22:57:21 +0200

    Error handling customization.

--- client/src/app/core/interceptors/error.interceptor.ts
+++ client/src/app/core/interceptors/error.interceptor.ts
@@ -1,6 +1,6 @@
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
-import { Router } from '@angular/router';
+import { Router, NavigationExtras } from '@angular/router';
 import { Injectable } from '@angular/core';
 import { catchError } from 'rxjs/operators';
 import { ToastrService } from 'ngx-toastr';
@@ -12,28 +12,33 @@ export class ErrorInterceptor implements HttpInterceptor {
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         return next.handle(req).pipe(
-            catchError(error => {
-                if (error) {
+            catchError(errorResponse => {
+                if (errorResponse) {
 
-                    if (error.status === 400) {
-                        this.toastr.error(error.error.message, error.error.statusCode);
+                    if (errorResponse.status === 400) {
+                        if (errorResponse.error.errors) {
+                            throw errorResponse.error;
+                        } else {
+                            this.toastr.error(errorResponse.error.message, errorResponse.error.statusCode);
+                        }
                     }
 
-                    if (error.status === 401) {
-                        this.toastr.error(error.error.message, error.error.statusCode);
+                    if (errorResponse.status === 401) {
+                        this.toastr.error(errorResponse.error.message, errorResponse.error.statusCode);
                     }
 
-                    if (error.status === 404) {
+                    if (errorResponse.status === 404) {
                         this.router.navigateByUrl('/not-found');
                     }
 
-                    if (error.status === 500) {
-                        this.router.navigateByUrl('/server-error');
+                    if (errorResponse.status === 500) {
+                        const navigationExtras: NavigationExtras = { state: {error: errorResponse.error}};
+                        this.router.navigateByUrl('/server-error', navigationExtras);
                     }
 
                 }
 
-                return throwError(error);
+                return throwError(errorResponse);
             })
         );
     }
--- client/src/app/core/test-error/test-error.component.ts
+++ client/src/app/core/test-error/test-error.component.ts
@@ -9,6 +9,7 @@ import { environment } from 'src/environments/environment';
 })
 export class TestErrorComponent implements OnInit {
   baseUrl = environment.apiUrl;
+  validationErrors: any;
 
   constructor(private http: HttpClient) { }
 
@@ -46,6 +47,7 @@ export class TestErrorComponent implements OnInit {
       console.log(response);
     }, errorResponse => {
       console.log(errorResponse);
+      this.validationErrors = errorResponse.errors;
     });
   }
 
